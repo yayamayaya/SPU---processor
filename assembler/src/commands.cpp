ed82600f11b136095d5d3f06e7dbfac288c4851a
yaya <semenov.aleksandr@phystech.edu>
2024-07-02 15:24:18 +0300

    массив структур для комманд

---
+++ assembler/src/commands.cpp
@@ -0,0 +1,41 @@
+#include "commands.h"
+
+cmd commands[COMMANDS_NUM] = {
+{"push", PUSH_GRP, 4},
+{"add", ADD, 3},
+{"sub", SUB, 3},
+{"mult", MULT, 4},
+{"div", DIV, 3},
+{"ret", RET, 3},
+{"out_i", OUTI, 5},
+{"out", OUT, 3},
+{"halt", HALT, 4},
+{"in", IN, 2},
+{"pop", POP_GRP, 3},
+{"jmp", JMP, 3},
+{"jb", JB, 2},
+{"jbe", JBE, 3},
+{"ja", JA, 2},
+{"jae", JAE, 3},
+{"je", JE, 2},
+{"jne", JNE, 3},
+{"call", CALL, 4},
+{"mov", MOV_GRP, 3},
+{"sqrt", SQRT, 4},
+{"pow", POW, 3},
+{"log_e", LOG_E, 5},
+{"log_ne", LOG_NE, 6},
+{"log_a", LOG_A, 5},
+{"log_ae", LOG_AE, 6},
+{"log_b", LOG_B, 5},
+{"log_be", LOG_BE, 6}
+//{"graph", GRAPHICS}
+};
+
+syntax_ch syntax_chars[SYNTAX_CH_NUMBER] =
+{
+    {'[', OP_SQ_BR},
+    {']', CL_SQ_BR},
+    {'+', PLUS},
+    {',', COMMA}
+};
---
+++ assembler/src/include/commands.h
@@ -0,0 +1,37 @@
+#include "asm.h"
+
+#ifndef ASM_CMDS
+#define ASM_CMDS
+
+#define COMMANDS_NUM 28
+#define REGISTERS_NUM 4
+
+#define SYNTAX_CH_NUMBER 4
+
+typedef struct
+{
+    const char *cmd_name;
+    enum MACHINE_CODE cmd_num;
+    unsigned char cmd_len;
+} cmd;
+
+enum SYNTAX_CH
+{
+    OP_SQ_BR    = 1,
+    CL_SQ_BR    = 2,
+    PLUS        = 3,
+    COMMA       = 4,
+
+    NO_SYNT     = 15
+};
+
+typedef struct
+{
+    const char syntax_char;
+    enum SYNTAX_CH char_code;
+} syntax_ch;
+
+extern cmd commands[COMMANDS_NUM];
+extern syntax_ch syntax_chars[SYNTAX_CH_NUMBER];
+
+#endif
