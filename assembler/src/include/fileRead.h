c63b8f14c543f94bcdf4a8b250f9b78b9af8a705
yaya <semenov.aleksandr@phystech.edu>
2024-07-02 15:22:36 +0300

    удалил старый ассемблер

--- assembler/src/include/fileRead.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-void sizeOfFile(FILE * fileName, int *fileSize);
-int readToBuff(const char *fileName, char **pcode, int *pfileSize);
-int spacesToZeroes(char *code, const int fileSize, int *wordCount);
-int breakToWords(char *code, char ***wordsPtrs, int *wordCount);
-int fileRead(const char *fileName, char ***strArrPtr, int *wordCount);
